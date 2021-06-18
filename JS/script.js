8443291b5b807216bd8e6b6bc9fc693396ef09ac
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-18 14:02:39 +0530

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,20 @@
 var li = document.querySelectorAll(".center li")
 var div = document.querySelectorAll(".id")
-var idv = document.querySelectorAll(".id div")
-console.log(li,div)
+var idv = document.querySelectorAll(".idv")
+var iidv = document.querySelectorAll(".iidv")
+console.log(div,idv,iidv)
 
 for(let i=0;i<idv.length;i++){
     idv[i].addEventListener("mouseover",function(){
         idv[i].classList.add("active")
-        div[i].classList.add("wide")
+        div[0].classList.add("wide")
+        iidv[i].classList.add("active")
     })
 }
 
 for(let i=0;i<idv.length;i++){
     idv[i].addEventListener("mouseout",function(){
-        idv[i].classList.remove("active")
-        div[i].classList.remove("wide")
+        iidv[i].classList.remove("active")
     })
 }
 
